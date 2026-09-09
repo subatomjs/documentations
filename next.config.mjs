17a0f6858817a9d6e478d30987e9fec39628707e
Kunal-Ch-Das-Official <kunalchandradasofficial@gmail.com>
2026-09-09 10:05:13 +0530

    Configure static export for Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  reactCompiler: true
+  reactCompiler: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
